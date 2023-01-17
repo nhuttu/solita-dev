1947a1e2864e3240eb190c0148115138c9e89766
nhuttu <nikohuttu1@gmail.com>
2023-01-17 22:10:39 +0200

    New home component added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./views/home";
 import Journey from "./views/journey/journey";
 import Journeys from "./views/journey/journeys";
 import Station from "./views/station/station";
@@ -8,6 +9,7 @@ import Stations from "./views/station/stations";
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/journeys" element={<Journeys />} />
       <Route path="/journey/:id" element={<Journey />} />
       <Route path="/stations" element={<Stations />} />
