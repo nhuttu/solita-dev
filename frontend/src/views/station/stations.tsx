aa81bd538934e2674be15af02d3d6e7d18d8f6f4
nhuttu <nikohuttu1@gmail.com>
2023-01-28 16:13:21 +0200

    File Modal added to stations.tsx

--- frontend/src/views/station/stations.tsx
+++ frontend/src/views/station/stations.tsx
@@ -1,15 +1,21 @@
 import { ChangeEvent, Fragment, useState } from "react";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
+import FileModal from "../../modals/file-modal";
 import { fetchStations } from "../../services/station.service";
 import { IStation } from "../../utils/types";
 import StationTable from "./station-table";
 
 const Stations = () => {
-  const { data, error, isFetching } = useQuery<IStation[] | undefined, Error>({
+  const { data, error, isFetching, status } = useQuery<
+    IStation[] | undefined,
+    Error
+  >({
     queryKey: ["stations"],
     queryFn: fetchStations,
   });
+
+  const [modalOpen, setModalOpen] = useState(false);
 
   const [filter, setFilter] = useState("");
 
@@ -18,7 +24,7 @@ const Stations = () => {
   };
 
   return (
-    <div className="min-h-full">
+    <div className="min-h-full ">
       {isFetching ? (
         <div className="flex items-center justify-center">Fetching</div>
       ) : error ? (
@@ -36,18 +42,27 @@ const Stations = () => {
               <Link to="/station">Create new station</Link>
             </button>
           </div>
-          <div className="flex  ">
-            <StationTable
-              stations={
-                data?.filter((station) => station.nameFI.includes(filter)) ?? []
-              }
-            />
-            )
+          <div className="flex justify-center">
+            {data.length > 0 && status === "success" ? (
+              <StationTable
+                stations={data.filter((station) =>
+                  station.nameFI.includes(filter)
+                )}
+              />
+            ) : (
+              <button
+                className="gap-3 rounded border-2 border-black"
+                onClick={() => setModalOpen(true)}
+              >
+                Download a CSV file
+              </button>
+            )}
           </div>
         </>
       ) : (
         <p>No data nor error</p>
       )}
+      {modalOpen && <FileModal setModalOpen={setModalOpen} />}
     </div>
   );
 };
